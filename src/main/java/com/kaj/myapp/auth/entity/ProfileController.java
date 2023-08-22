68ca4745e02215947d587d57b93fda8c4b50e25d
Aejin Kang <dowlsdl003@naver.com>
2023-08-22 16:23:48 +0900

    프로필 삭제 기능 추가

--- src/main/java/com/kaj/myapp/auth/entity/ProfileController.java
+++ src/main/java/com/kaj/myapp/auth/entity/ProfileController.java
@@ -78,5 +78,15 @@ public class ProfileController {
 
         return ResponseEntity.ok().build();
     }
+    @Auth
+    @DeleteMapping(value = "/{no}")
+    public ResponseEntity deleteProfile(@PathVariable long no, @RequestAttribute AuthUser authUser) {
+        Optional<Profile> findPro = proRepo.findById(no);
+        if(!findPro.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        proRepo.deleteById(no);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 
 }
